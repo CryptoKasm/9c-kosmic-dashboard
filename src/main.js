13e7d032b17e6dbf885f079dfddd77a6cb89b100
Blaque Allen <cryptokasm@gmail.com>
2021-05-21 02:28:12 +0000

    Added FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).use(router).mount("#app");
+library.add(fas);
+
+createApp(App).use(router).component("fa", FontAwesomeIcon).mount("#app");
