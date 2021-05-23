4113070ede74facf578493e0191760609eae6c3f
Blaque Allen <cryptokasm@gmail.com>
2021-05-22 20:50:32 -0500

    Added page views

---
+++ src/views/BugReport.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3>Bug-Report</h3>
+</template>
+
+<script>
+export default {
+  name: "BugReport",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/default-theme.scss";
+
+h3 {
+  flex-grow: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0;
+  padding: 0;
+  height: 54px;
+  text-align: left;
+  color: white;
+  @include DesktopDisplay-Large-Bold;
+}
+</style>
---
+++ src/views/Settings.vue
@@ -0,0 +1,28 @@
+<template>
+  <h3>Settings</h3>
+</template>
+
+<script>
+export default {
+  name: "Settings",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/default-theme.scss";
+
+h3 {
+  flex-grow: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0;
+  padding: 0;
+  height: 54px;
+  text-align: left;
+  color: white;
+  @include DesktopDisplay-Large-Bold;
+}
+</style>
