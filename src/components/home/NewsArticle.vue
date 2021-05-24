e0b4cd93dca937247add38952412aeaf4c656652
Blaque Allen <cryptokasm@gmail.com>
2021-05-24 14:21:32 -0500

    Added NewsArticle component

---
+++ src/components/home/NewsArticle.vue
@@ -0,0 +1,181 @@
+<template>
+  <a :href="articleURL" target="_blank" class="article-wrapper">
+    <div class="article">
+      <div class="image-shape">
+        <img alt="" class="image" :src="imageSrc" />
+      </div>
+      <div class="article-info">
+        <div class="meta-info">
+          <div class="title-text">{{ titleText }}</div>
+          <div class="sub-info">
+            <p class="source-text">Source: {{ sourceText }}</p>
+            <p class="date-text">{{ dateText }}</p>
+          </div>
+        </div>
+        <p class="summary-text">
+          {{ summaryText }}
+        </p>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "NewsArticle",
+  props: {
+    imageSrc: String,
+    articleURL: String,
+    titleText: String,
+    sourceText: String,
+    dateText: String,
+    summaryText: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/default-theme.scss";
+
+.article-wrapper {
+  width: 424px;
+  height: 230px;
+  flex-grow: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 0;
+  padding: 1px;
+  border-radius: 15px;
+  background-color: $dark-button-component;
+  margin: 0 auto;
+  text-decoration: none;
+}
+
+.article-wrapper:first-child {
+  margin-top: 0 !important;
+  margin-left: 0 !important;
+}
+
+.article-wrapper:last-child {
+  margin-bottom: 0 !important;
+  margin-right: 0 !important;
+}
+
+.article-wrapper:hover {
+  background: linear-gradient(
+      -90deg,
+      $dark-button-component,
+      $dark-button-component
+    ),
+    linear-gradient(90deg, $dark-button-component 0%, #11a8fd 100%);
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+}
+
+.article-wrapper:active {
+  filter: brightness(140%);
+}
+
+.article {
+  flex-grow: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 0;
+  padding: 12px 16px 12px 12px;
+  border-radius: 15px;
+  margin: 0 auto;
+  text-decoration: none;
+}
+
+.image-shape {
+  width: 160px;
+  height: 200px;
+  margin-right: 12px;
+}
+
+.image-shape img {
+  width: inherit;
+  height: inherit;
+  border-radius: 15px;
+  object-fit: cover;
+}
+
+.article-info {
+  align-self: stretch;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  gap: 0;
+  padding: 12px 0 8px 0;
+}
+
+.meta-info {
+  align-self: stretch;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 8px;
+  padding: 0;
+  align-items: center;
+}
+
+.title-text {
+  align-self: stretch;
+  flex-grow: 0;
+  font-family: Aller;
+  font-size: 22px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: $grayscale-off-white;
+  text-decoration: none;
+}
+
+.sub-info {
+  flex-grow: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 52px;
+  padding: 0 0 4px 0;
+  font-family: Aller;
+  font-size: 11px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+}
+
+.source-text,
+.date-text {
+  flex-grow: 0;
+  text-align: left;
+  color: $grayscale-off-white;
+}
+
+.summary-text {
+  align-self: stretch;
+  flex-grow: 1;
+  font-family: Aller;
+  font-size: 10px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: $grayscale-off-white;
+  padding: 0 4px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,122 @@
 <template>
-  <h3>Home</h3>
+  <div class="content-frame">
+    <div class="news-articles">
+      <!-- Need to add for loop, for each news article in rss feed -->
+      <NewsArticle
+        imageSrc="https://cdn-images-1.medium.com/max/1024/0*mVkJIoGjJcvEbsww.png"
+        articleURL="https://ninechronicles.medium.com/monster-collection-muspelheim-the-realm-of-fire-part-2-b5c36e089b81"
+        titleText="Monster Collection: Muspelheim, The Realm of Fire Part 2"
+        sourceText="Medium"
+        dateText="5/21/2021"
+        summaryText="The second
+        chapter begins with the opening of the 5th world known as Muspelheim, the
+        Realm of Fire. Being the source of heat in all Nine Realms, Muspelheim
+        introduces a completely new concept of Lizard monsters and a new glorious
+        battlefield."
+      />
+      <NewsArticle
+        imageSrc="https://cdn-images-1.medium.com/max/1024/0*mVkJIoGjJcvEbsww.png"
+        articleURL="https://ninechronicles.medium.com/muspelheim-the-realm-of-fire-is-coming-soon-35a959c3fb20"
+        titleText="Muspelheim, The Realm of Fire is coming soon"
+        sourceText="Medium"
+        dateText="5/18/2021"
+        summaryText="The second chapter begins with the opening of the 5th world known as 
+        Muspelheim, the Realm of Fire. Being the source of heat in all Nine Realms, 
+        Muspelheim introduces a completely new concept of Lizard monsters and a new 
+        glorious battlefield."
+      />
+      <NewsArticle
+        imageSrc="https://cdn-images-1.medium.com/max/1024/0*0TRjyCajKGnsK9aJ"
+        articleURL="https://ninechronicles.medium.com/development-update-blog-april-2021-21f1385fb1c"
+        titleText="Muspelheim, The Realm of Fire is coming soon"
+        sourceText="Medium"
+        dateText="5/4/2021"
+        summaryText="This month saw a number of small quality-of-life fixes, although 
+        one major update this month was the roll out of the new Marketplace UI. 
+        We also worked on a number of long-term features that are not yet ready for deployment."
+      />
+    </div>
+    <div class="leaderboards"></div>
+    <div class="network-cards">
+      <div class="row-frame"></div>
+      <div class="row-frame"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import NewsArticle from "@/components/home/NewsArticle.vue";
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    NewsArticle,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/scss/default-theme.scss";
 
-h3 {
-  flex-grow: 0;
+.content-frame {
+  width: 1300px;
+  align-self: stretch;
+  flex-grow: 1;
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  gap: 32px;
+  padding: 8px 0 24px;
+  margin: 0 auto;
+}
+
+.news-articles {
+  align-self: stretch;
+  flex-grow: 0;
+  display: flex;
+  flex-direction: row;
   justify-content: center;
-  align-items: center;
+  align-items: stretch;
   gap: 0;
   padding: 0;
-  height: 54px;
-  text-align: left;
-  color: white;
-  @include DesktopDisplay-Large-Bold;
+  // background-color: green;
+}
+
+.leaderboards {
+  align-self: stretch;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: stretch;
+  gap: 24px;
+  padding: 0;
+  // background-color: green;
+}
+
+.network-cards {
+  align-self: stretch;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  gap: 24px;
+  padding: 0;
+  // background-color: green;
+}
+
+.row-frame {
+  // height: 94px;
+  align-self: stretch;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: stretch;
+  gap: 24px;
+  padding: 0;
+  // background-color: #ff5050;
 }
 </style>
