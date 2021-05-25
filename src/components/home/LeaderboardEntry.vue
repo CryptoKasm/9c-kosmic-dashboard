346a4e35319fe81b28a5a4fbbc5d7d93928d59c0
Blaque Allen <cryptokasm@gmail.com>
2021-05-24 22:50:10 -0500

    Added Leaderboards

---
+++ src/components/home/LeaderboardEntry.vue
@@ -0,0 +1,217 @@
+<template>
+  <button class="entry">
+    <div class="entry-wrapper">
+      <div class="rank-field">
+        <p class="rank-text">{{ accountRank }}</p>
+      </div>
+      <div class="profile-img-field">
+        <img
+          :alt="accountProfileImgAlt"
+          class="mask-shape"
+          :src="accountProfileImg"
+        />
+      </div>
+      <div class="profile-field">
+        <div class="account-field">
+          <p class="account-text">{{ accountPublicID }}</p>
+        </div>
+        <div class="character-field">
+          <div
+            class="character-name"
+            v-for="characterName in accountCharacters"
+            :value="characterName.value"
+            :key="characterName.value"
+          >
+            <p class="name-text">{{ characterName }}</p>
+          </div>
+          <!-- <div class="character-name">
+            <p class="name-text">TripLo #7df3</p>
+          </div>
+          <div class="character-name">
+            <p class="name-text">Fuxwithme #7df3</p>
+          </div>
+          <div class="character-name">
+            <p class="name-text">EMPTY</p>
+          </div> -->
+        </div>
+      </div>
+      <div class="data-field">
+        <div class="databg">
+          <!-- Query: account gold balance -->
+          <p v-if="accountNCG" class="data-text">{{ accountNCG }} NCG</p>
+
+          <!-- Query: account total xp -->
+          <p v-if="accountXP" class="data-text">{{ accountXP }} XP</p>
+        </div>
+      </div>
+    </div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Leaderboard-Entry",
+  props: {
+    accountPublicID: String,
+    accountCharacters: Array,
+    accountRank: Number,
+    accountProfileImg: String,
+    accountNCG: Number,
+    accountXP: Number,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/default-theme.scss";
+
+.entry {
+  height: 48px;
+  overflow: hidden;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  align-self: stretch;
+  position: relative;
+  padding: 1px;
+  background-color: $dark-secondary-component;
+}
+
+.entry:hover {
+  background: linear-gradient(
+      90deg,
+      $dark-secondary-component,
+      $dark-secondary-component
+    ),
+    linear-gradient(90deg, $dark-secondary-component 0%, #11a8fd 100%);
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+}
+
+.entry:active {
+  filter: brightness(120%);
+}
+
+.entry-wrapper {
+  overflow: hidden;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  align-self: stretch;
+}
+
+.rank-field {
+  padding: 0 12px 0 12px;
+  display: flex;
+  align-items: center;
+  align-self: stretch;
+}
+
+.rank-text {
+  font-family: "Aller";
+  font-size: 22px;
+  font-weight: 700;
+  line-height: normal;
+  color: rgba(255, 255, 255, 1);
+  opacity: 0.3;
+}
+
+.profile-img-field {
+  padding: 0 16px 0 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-self: stretch;
+}
+
+.mask-shape {
+  width: 32px;
+  height: 32px;
+}
+
+.profile-field {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  flex: 1;
+  align-self: stretch;
+}
+
+.account-field {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  flex: 0;
+  align-self: stretch;
+}
+
+.account-text {
+  font-family: Aller;
+  font-size: 13px;
+  font-weight: bold;
+  line-height: 1.71;
+  letter-spacing: 0.75px;
+  text-decoration: none;
+  color: rgba(255, 255, 255, 1);
+}
+
+.character-field {
+  padding: 0 0 0 8px;
+  display: flex;
+  align-items: center;
+  align-self: stretch;
+}
+
+.character-name {
+  background-color: rgba(25, 36, 57, 0.9);
+  overflow: hidden;
+  border-radius: 5px;
+  padding: 0 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 16px;
+  &:not(:last-of-type) {
+    margin-right: 16px;
+  }
+}
+
+.name-text {
+  font-family: "Pangram";
+  font-size: 10px;
+  font-weight: 400;
+  line-height: normal;
+  color: rgba(28, 146, 255, 1);
+}
+
+.data-field {
+  padding: 0 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  right: 0;
+}
+
+.databg {
+  background-color: rgba(25, 36, 57, 0.9);
+  overflow: hidden;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 30px;
+  width: 112px;
+}
+
+.data-text {
+  font-family: "Pangram";
+  font-size: 12px;
+  font-weight: 400;
+  line-height: normal;
+  color: rgba(28, 146, 255, 1);
+}
+</style>
---
+++ src/components/home/XPLeaderboard.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="leaderboard-frame">
+    <div class="header">
+      <div class="left">
+        <p class="title-text">Most Experienced Players</p>
+      </div>
+      <div class="right">
+        <div class="category-text">
+          <p class="leaderboard-text">LEADERBOARD</p>
+        </div>
+      </div>
+    </div>
+    <div class="line-field"><div class="line"></div></div>
+    <div class="entries-field">
+      <div
+        class="entries-list"
+        v-for="account in accounts"
+        :value="account.value"
+        :key="account.value"
+      >
+        <LeaderboardEntry
+          :accountPublicID="account.PublicID"
+          :accountCharacters="account.Characters"
+          :accountProfileImg="account.ProfileImg"
+          :accountRank="account.Rank"
+          :accountXP="account.XP"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LeaderboardEntry from "@/components/home/LeaderboardEntry.vue";
+
+export default {
+  name: "XPLeaderboard",
+  components: {
+    LeaderboardEntry,
+  },
+  data() {
+    return {
+      accounts: [
+        {
+          PublicID: "0xf5eB9FCbe5D2A658A87b517511f14698ADd28Efa",
+          Characters: [ "TripLo #7r40", "Fuxwithme #8f9e", "EMPTY" ],
+          ProfileImg:
+            "https://static.overlay-tech.com/assets/9bec0aad-cae7-4730-831e-577cff246de6.png",
+          Rank: "01",
+          XP: "340,504",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/default-theme.scss";
+
+.leaderboard-frame {
+  width: 647px;
+  flex-grow: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  gap: 0;
+  padding: 2px 0 0;
+  border-radius: 9px;
+  background-color: $dark-button-component;
+}
+
+.header {
+  min-height: 48px;
+  align-self: stretch;
+  flex-grow: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: stretch;
+  gap: 0;
+  padding: 0 24px;
+}
+
+.left {
+  align-self: stretch;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 0;
+  padding: 0;
+}
+
+.title-text {
+  flex-grow: 0;
+  font-family: Aller;
+  font-size: 18px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #ffffff;
+}
+
+.right {
+  align-self: stretch;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 0;
+  padding: 0;
+}
+
+.category-text {
+  width: 113px;
+  height: 32px;
+  flex-grow: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  padding: 0;
+  border-radius: 5px;
+  border: solid 0.5px #ffffff;
+}
+
+.leaderboard-text {
+  flex-grow: 0;
+  opacity: 0.5;
+  font-family: Aller;
+  font-size: 12px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: left;
+  color: #ffffff;
+}
+
+.line-field {
+  height: 2px;
+  align-self: stretch;
+  flex-grow: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 0;
+  padding: 0;
+}
+
+.line {
+  height: 1px;
+  flex-grow: 1;
+  opacity: 0.05;
+  background-color: #ffffff;
+}
+
+.entries-field {
+  padding: 12px 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  align-self: stretch;
+  gap: 8px;
+}
+
+.entries-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 0;
+  align-self: stretch;
+}
+</style>
